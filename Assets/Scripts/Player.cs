26439c0665d318d2dabc910e5ada264160a1de41
SHsus1122 <tjgurtn1122@naver.com>
2024-08-05 21:40:08 +0900

    에셋 임포트 및 기초 움직임 구현

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    float hAxis;
+    float vAxis;
+    bool wDown;
+
+    Vector3 moveVec;
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");  // 가로축 입력
+        vAxis = Input.GetAxisRaw("Vertical");      // 세로축 입력
+        wDown = Input.GetButton("Walk");
+
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
+
+        anim.SetBool("isRun", moveVec != Vector3.zero);
+        anim.SetBool("isWalk", wDown);
+
+        transform.LookAt(transform.position + moveVec);
+    }
+}
