c859d6b4752de4e6179413df49bfdc6dc1652345
SHsus1122 <tjgurtn1122@naver.com>
2024-08-12 10:37:20 +0900

    다양한 패턴을 구사하는 보스 만들기

--- Assets/2Scripts/Player.cs
+++ Assets/2Scripts/Player.cs
@@ -334,22 +334,26 @@ public class Player : MonoBehaviour
             {
                 Bullet enemyBullet = other.GetComponent<Bullet>();
                 health -= enemyBullet.damage;
-                
-                if (other.GetComponent<Rigidbody>() != null)
-                    Destroy(other.gameObject);
 
-                StartCoroutine(OnDamage());
+                bool isBossAttack = other.name == "Boss Melee Area";
+                StartCoroutine(OnDamage(isBossAttack));
             }
+
+            if (other.GetComponent<Rigidbody>() != null)
+                Destroy(other.gameObject);
         }
     }
 
-    IEnumerator OnDamage()
+    IEnumerator OnDamage(bool isBossAtk)
     {
         isDamage = true;
         foreach (MeshRenderer mesh in meshs)
         {
             mesh.material.color = Color.yellow;
         }
+
+        if (isBossAtk)
+            rigid.AddForce(transform.forward * -25, ForceMode.Impulse);
 
         yield return new WaitForSeconds(1f);
 
@@ -359,6 +363,8 @@ public class Player : MonoBehaviour
             mesh.material.color = Color.white;
         }
 
+        if (isBossAtk)
+            rigid.velocity = Vector3.zero;
     }
 
     void OnTriggerStay(Collider other)
